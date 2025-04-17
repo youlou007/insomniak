1095f920ac527fbdc7648a4fc9382615a761e3d5
Hamitouche Yacine <44175867+youlou007@users.noreply.github.com>
2025-04-18 00:54:52 +0200

    Updates

---
+++ _next/static/VrLGfR4Kc7N3VCnQaMmWu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/198-40b1d5d98b9ba9e1.js","static/chunks/pages/index-0c2b6ac6a9c7c969.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
