26a7ae5f7e8974f270181308112c30c230225ac7
Hamitouche Yacine <44175867+youlou007@users.noreply.github.com>
2025-04-19 05:16:24 +0200

    Updates

--- _next/static/Qyx6BE5BGkcSXWIoQT0Nj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/198-62aa8a052fccda12.js","static/chunks/pages/index-fe4f15fe4456bd51.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
