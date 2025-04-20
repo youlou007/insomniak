5c253e64799c102a0be727541015a324926efdb0
youlou007 <44175867+youlou007@users.noreply.github.com>
2025-04-20 21:57:50 +0000

    Deploying to gh-pages from @ youlou007/insomniak@5b988ae6c4ee62611fb34b53ae5669c16259221e 🚀

--- _next/static/vdPvyWpRytceGa8uHay6w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/972-e2b06849a20ccad6.js","static/chunks/pages/index-f2e201129edb1bad.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
